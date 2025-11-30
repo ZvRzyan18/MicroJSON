fd000505ccba49de2d8f17756c7ed19bcdd8ee5d
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-11-30 15:54:13 +0800

    update into 0.0.2

--- src/micro_json/object.c
+++ src/micro_json/object.c
@@ -2,13 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+/*-----------------Static func-------------------*/
+
 static int fast_abs32(int x) {
  int mask = x >> 31;
  return (x ^ mask) - mask;
 }
 
 
-//-----------------MJSArray-------------------//
+/*-----------------MJSArray-------------------*/
 /*
  allocate MJSArray object, return 0 if success, return -1 if not.
 */
@@ -29,15 +32,17 @@ int MJSArray_Init(MJSArray *arr) {
 int MJSArray_Destroy(MJSArray *arr) {
  if(!arr)
   return -1;
- //delete them first, to make sure no memory leaks
- int i;
+ /*
+  delete them first, to make sure no memory leaks
+ */
+ unsigned int i;
  for(i = 0; i < arr->size; i++) {
   switch(arr->dynamic_type_ptr[i].type) {
    case MJS_TYPE_ARRAY:
-    if(!MJSArray_Destroy(&arr->dynamic_type_ptr[i].value_array)) return -1;
+    if(MJSArray_Destroy(&arr->dynamic_type_ptr[i].value_array)) return -1;
    break;
-   case MJS_TYPE_CONTAINER:
-    if(!MJSContainer_Destroy(&arr->dynamic_type_ptr[i].value_container)) return -1;
+   case MJS_TYPE_OBJECT:
+    if(MJSObject_Destroy(&arr->dynamic_type_ptr[i].value_object)) return -1;
    break;
   }
  }
@@ -66,7 +71,7 @@ int MJSArray_Add(MJSArray *arr, MJSDynamicType value) {
   arr->dynamic_type_ptr = new_ptr;
   arr->reserve = MAX_RESERVE_ELEMENTS;
   
-  //add value
+  /* add value */
   arr->dynamic_type_ptr[arr->size] = value;
   arr->reserve--;
   arr->size++;
@@ -94,23 +99,24 @@ unsigned int MJSArray_Size(MJSArray *arr) {
 }
 
 
-//-----------------MJSContainer-------------------//
+/*-----------------MJSContainer-------------------*/
 static unsigned int generate_hash_index(const char *str, unsigned int str_size);
-static unsigned int find_next_empty_slot(MJSContainer *container);
 
-int MJSContainer_Init(MJSContainer *container) {
+
+int MJSObject_Init(MJSObject *container) {
  if(!container)
   return -1;
- memset(container, 0x00, sizeof(MJSContainer));
+ memset(container, 0x00, sizeof(MJSObject));
  
  container->obj_pair_ptr = (MJSObjectPair*)malloc(sizeof(MJSObjectPair) * MAX_RESERVE_ELEMENTS);
  if(!container->obj_pair_ptr)
   return -1;
  memset(container->obj_pair_ptr, 0xFF, MAX_RESERVE_ELEMENTS * sizeof(MJSObjectPair));
  
- container->reserve = MAX_RESERVE_ELEMENTS;
- container->allocated_size = MAX_RESERVE_ELEMENTS;
- container->string_pool = (char*)malloc(MAX_RESERVE_BYTES);
+ container->reserve = MAX_HASH_BUCKETS+MAX_RESERVE_ELEMENTS;
+ container->allocated_size = MAX_HASH_BUCKETS+MAX_RESERVE_ELEMENTS;
+ container->next_empty = MAX_HASH_BUCKETS;
+ container->string_pool = (char*)malloc(sizeof(char) * MAX_RESERVE_BYTES);
  if(!container->string_pool)
   return -1;
  container->string_pool_reserve = MAX_RESERVE_BYTES;
@@ -118,18 +124,18 @@ int MJSContainer_Init(MJSContainer *container) {
 }
 
 
-int MJSContainer_Destroy(MJSContainer *container) {
+int MJSObject_Destroy(MJSObject *container) {
  if(!container)
   return -1;
- //destroy other allocated memory first.
- int i;
+ /* destroy other allocated memory first. */
+ unsigned int i;
  for(i = 0; i < container->obj_pair_size; i++) {
   switch(container->obj_pair_ptr[i].value.type) {
    case MJS_TYPE_ARRAY:
-    if(!MJSArray_Destroy(&container->obj_pair_ptr[i].value.value_array)) return -1;
+    if(MJSArray_Destroy(&container->obj_pair_ptr[i].value.value_array)) return -1;
    break;
-   case MJS_TYPE_CONTAINER:
-    if(!MJSContainer_Destroy(&container->obj_pair_ptr[i].value.value_container)) return -1;
+   case MJS_TYPE_OBJECT:
+    if(MJSObject_Destroy(&container->obj_pair_ptr[i].value.value_object)) return -1;
    break;
   }
  }
@@ -139,20 +145,22 @@ int MJSContainer_Destroy(MJSContainer *container) {
 }
 
 
-unsigned int MJSContainer_GetSize(MJSContainer *container) {
+unsigned int MJSObject_GetSize(MJSObject *container) {
  if(!container)
   return 0xFFFFFFFF;
  return container->obj_pair_size;
 }
 
-int MJSContainer_InsertFromPool(MJSContainer *container, unsigned int pool_index, unsigned int str_size, MJSDynamicType value) {
+
+
+int MJSObject_InsertFromPool(MJSObject *container, unsigned int pool_index, unsigned int str_size, MJSDynamicType value) {
  if(!container || (pool_index > container->string_pool_size) || !str_size)
   return -1;
  char *key = &container->string_pool[pool_index];
  unsigned int str_pool_index = pool_index;
  MJSObjectPair pair;
  
- memset(&pair, 0x00, sizeof(MJSObjectPair));
+ memset(&pair, 0xFF, sizeof(MJSObjectPair));
  pair.key_pool_index = str_pool_index;
  pair.key_pool_size = str_size;
  pair.value = value;
@@ -171,7 +179,7 @@ int MJSContainer_InsertFromPool(MJSContainer *container, unsigned int pool_index
 	 free(container->obj_pair_ptr);
 	 container->obj_pair_ptr = new_obj;
 	}
-			
+	
 	MJSObjectPair *start_node = &container->obj_pair_ptr[hash_index];
 	
 	if(start_node->key_pool_index == 0xFFFFFFFF) {
@@ -180,19 +188,21 @@ int MJSContainer_InsertFromPool(MJSContainer *container, unsigned int pool_index
 	 unsigned int prev_index = hash_index;
   unsigned int next_index = hash_index;
  	do {
-	 	prev_index = next_index;
+ 		prev_index = next_index;
 	 	
-   if(start_node->key_pool_index == 0xFFFFFFFF) {
+   if(start_node->next == 0xFFFFFFFF) {
 			 prev_index = next_index;
-		 	next_index = find_next_empty_slot(container);
+		 	next_index = container->next_empty++;
+		 	if(next_index == 0xFFFFFFFF)
+		 	 return -1;
 		 	container->obj_pair_ptr[next_index] = pair;
 		  container->obj_pair_ptr[prev_index].next = next_index;
 	 	 break;
 	 	}
+	 	
 		 next_index = start_node->next;
-	 	start_node = &container->obj_pair_ptr[start_node->next];
+	  start_node = &container->obj_pair_ptr[next_index];
  	} while(1);
-
 	}
 	container->reserve--;
 	container->obj_pair_size++;
@@ -201,11 +211,11 @@ int MJSContainer_InsertFromPool(MJSContainer *container, unsigned int pool_index
 }
 
 
-int MJSContainer_Insert(MJSContainer *container, const char *key, unsigned int str_size, MJSDynamicType value) {
+int MJSObject_Insert(MJSObject *container, const char *key, unsigned int str_size, MJSDynamicType value) {
  if(!container || !key || !str_size)
   return -1;
  
- unsigned int str_pool_index = MJSContainer_AddToStringPool(container, key, str_size);
+ unsigned int str_pool_index = MJSObject_AddToStringPool(container, key, str_size);
  MJSObjectPair pair;
  
  memset(&pair, 0x00, sizeof(MJSObjectPair));
@@ -236,17 +246,20 @@ int MJSContainer_Insert(MJSContainer *container, const char *key, unsigned int s
 	 unsigned int prev_index = hash_index;
   unsigned int next_index = hash_index;
  	do {
-	 	prev_index = next_index;
+ 		prev_index = next_index;
 	 	
-   if(start_node->key_pool_index == 0xFFFFFFFF) {
+   if(start_node->next == 0xFFFFFFFF) {
 			 prev_index = next_index;
-		 	next_index = find_next_empty_slot(container);
+		 	next_index = container->next_empty++;
+		 	if(next_index == 0xFFFFFFFF)
+		 	 return -1;
 		 	container->obj_pair_ptr[next_index] = pair;
 		  container->obj_pair_ptr[prev_index].next = next_index;
 	 	 break;
 	 	}
+	 	
 		 next_index = start_node->next;
-	 	start_node = &container->obj_pair_ptr[start_node->next];
+	  start_node = &container->obj_pair_ptr[next_index];
  	} while(1);
 
 	}
@@ -257,7 +270,7 @@ int MJSContainer_Insert(MJSContainer *container, const char *key, unsigned int s
 }
 
 
-MJSObjectPair* MJSContainer_GetPairReference(MJSContainer *container, const char *key) {
+MJSObjectPair* MJSObject_GetPairReference(MJSObject *container, const char *key) {
  if(!container || !key)
   return NULL;
   
@@ -288,32 +301,65 @@ MJSObjectPair* MJSContainer_GetPairReference(MJSContainer *container, const char
 }
 
 
-unsigned int MJSContainer_AddToStringPool(MJSContainer *container, const char *str, unsigned int str_size) {
+MJSObjectPair* MJSObject_GetPairReferenceFromPool(MJSObject *container, unsigned int pool_index, unsigned int str_size) {
+ if(!container || (pool_index > container->string_pool_size))
+  return NULL;
+
+ char *key = &container->string_pool[pool_index];
+ const unsigned int key_len = str_size;
+ const unsigned int hash_index = generate_hash_index(key, key_len);
+ 
+ if(!container->obj_pair_size)
+  return NULL;
+  
+	MJSObjectPair *start_node = &container->obj_pair_ptr[hash_index];
+	
+	if(start_node->key_pool_index == 0xFFFFFFFF)
+	 return NULL;
+ else {
+  unsigned int next_index = hash_index;
+ 	do {
+ 	
+ 	 if(key_len == start_node->key_pool_size) {
+    if(memcmp(key, &container->string_pool[start_node->key_pool_index], key_len) == 0) {
+	 	  return start_node;
+	 	 }
+ 	 }
+		 next_index = start_node->next;
+	 	start_node = &container->obj_pair_ptr[start_node->next];
+ 	} while(1);
+	}
+ return NULL;
+}
+
+
+
+unsigned int MJSObject_AddToStringPool(MJSObject *container, const char *str, unsigned int str_size) {
  if(!container || !str || !str_size)
   return 0xFFFFFFFF;
   
  unsigned int out_index = 0xFFFFFFFF;
  
  if(container->string_pool_reserve > (str_size+1)) {
-  memcpy(&container->string_pool[container->string_pool_size], str, str_size);
+  memcpy(&container->string_pool[container->string_pool_size], str, sizeof(char) * str_size);
   out_index = container->string_pool_size;
   container->string_pool_size += str_size;
   container->string_pool_reserve -= str_size;
-  //null terminatior
+  /* null terminatior */
   container->string_pool[container->string_pool_size++] = '\0';
   return out_index;
  } else {
-  char *new_str = (char*)malloc(container->string_pool_size + (str_size+1) + MAX_RESERVE_ELEMENTS);
+  char *new_str = (char*)malloc(sizeof(char) * (container->string_pool_size + (str_size+1) + MAX_RESERVE_ELEMENTS));
   if(!new_str)
    return 0xFFFFFFFF;
-  memcpy(new_str, container->string_pool, container->string_pool_size);
+  memcpy(new_str, container->string_pool, sizeof(char) * container->string_pool_size);
   free(container->string_pool);
   container->string_pool = new_str;
-  memcpy(&container->string_pool[container->string_pool_size], str, str_size);
+  memcpy(&container->string_pool[container->string_pool_size], str, sizeof(char) * str_size);
   out_index = container->string_pool_size;
   container->string_pool_size += str_size;
   container->string_pool_reserve = MAX_RESERVE_ELEMENTS;
-  //null terminatior
+  /* null terminatior */
   container->string_pool[container->string_pool_size++] = '\0';
   return out_index;
  }
@@ -321,32 +367,28 @@ unsigned int MJSContainer_AddToStringPool(MJSContainer *container, const char *s
 }
 
 
-//-----------------Static func-------------------//
-//Hash Function
-//TODO : optimize this, mininize loop branches
+const char* MJSObject_GetStringFromPool(MJSObject *container, MJSString *str) {
+ if(!container || !str)
+  return NULL;
+ return &container->string_pool[str->pool_index];
+}
+
+/*-----------------Static func-------------------*/
+/*
+ Hash Function
+ TODO : optimize this, mininize loop branches
+ or use agressive unrolling
+*/
 static unsigned int generate_hash_index(const char *str, unsigned int str_size) {
  unsigned int index = 0;
- int i;
+ unsigned int i;
  for(i = 0; i < str_size; i++)
   index += (unsigned int)str[i];
  return index % MAX_HASH_BUCKETS;
 }
 
-//Linear Probing
-//TODO : optimize this, mininize loop branches
-static unsigned int find_next_empty_slot(MJSContainer *container) {
- unsigned int index = MAX_HASH_BUCKETS-1;
- while(index < container->allocated_size) {
-  if(container->obj_pair_ptr[index].key_pool_index == 0xFFFFFFFF)
-   return index;
-  index++;
- }
- return 0xFFFFFFFF;
-}
 
-
-
-//-----------------MJSParser-------------------//
+/*-----------------MJSParser-------------------*/
 /*
  allocate MJSParserData, return 0 if sucess, return -1 if not.
 */
@@ -355,7 +397,7 @@ int MJSParserData_Create(MJSParsedData *parsed_data) {
   return -1;
  
  memset(parsed_data, 0, sizeof(MJSParsedData));
- parsed_data->cache = (unsigned char*)malloc(MAX_CACHE_BYTES);
+ parsed_data->cache = (unsigned char*)malloc(sizeof(unsigned char) * MAX_CACHE_BYTES);
  parsed_data->cache_allocated_size = MAX_CACHE_BYTES;
  
  if(!parsed_data->cache)
@@ -369,10 +411,24 @@ int MJSParserData_Create(MJSParsedData *parsed_data) {
 int MJSParserData_Destroy(MJSParsedData *parsed_data) {
  if(!parsed_data)
   return -1;
- MJSContainer_Destroy(&parsed_data->container);
+ MJSObject_Destroy(&parsed_data->container);
  free(parsed_data->cache);
  return 0;
 }
 
 
+int MJSParserData_ExpandCache(MJSParsedData *parsed_data) {
+ if(!parsed_data)
+  return -1;
+ unsigned char *new_cache = (unsigned char*)malloc(sizeof(unsigned char) * (parsed_data->cache_allocated_size + MAX_RESERVE_BYTES));
+ 
+ if(!new_cache)
+  return -1;
+  
+ memcpy(new_cache, parsed_data->cache, sizeof(unsigned char) * parsed_data->cache_size);
+ free(parsed_data->cache);
+ parsed_data->cache = new_cache;
+ parsed_data->cache_allocated_size += MAX_RESERVE_BYTES;
+ return 0;
+}
 
--- src/micro_json/parser.c
+++ src/micro_json/parser.c
@@ -1,11 +1,50 @@
 #include "micro_json/parser.h"
 #include <string.h>
+#include <stdlib.h>
 #include <math.h>
 
-//return if its a valid whitespace or not
+
+/*convert error code to string */
+const char* MJS_CodeToString(signed char code) {
+ switch(code) {
+  case MJS_RESULT_NO_ERROR:
+   return "MJS_RESULT_NO_ERROR";
+  break;
+  case MJS_RESULT_NULL_POINTER:
+   return "MJS_RESULT_NULL_POINTER";
+  break;
+  case MJS_RESULT_SYNTAX_ERROR:
+   return "MJS_RESULT_SYNTAX_ERROR";
+  break;
+  case MJS_RESULT_UNEXPECTED_TOKEN:
+   return "MJS_RESULT_UNEXPECTED_TOKEN";
+  break;
+  case MJS_RESULT_INCOMPLETE_BRACKETS:
+   return "MJS_RESULT_INCOMPLETE_BRACKETS";
+  break;
+  case MJS_RESULT_INCOMPLETE_DOUBLE_QUOTES:
+   return "MJS_RESULT_INCOMPLETE_DOUBLE_QUOTES";
+  break;
+  case MJS_RESULT_ALLOCATION_FAILED:
+   return "MJS_RESULT_ALLOCATION_FAILED";
+  break;
+  case MJS_RESULT_INVALID_TYPE:
+   return "MJS_RESULT_INVALID_TYPE";
+  break;
+  case MJS_RESULT_REACHED_MAX_NESTED_DEPTH:
+   return "MJS_RESULT_REACHED_MAX_NESTED_DEPTH";
+  break;
+ }
+ return "Unknown Error";
+}
+
+
+/* return if its a valid whitespace or not */
 int MJS_IsWhiteSpace(char c) {
  return (c == '\0') || (c == 0x20) || (c == 0x0A) || (c == 0x0D) || (c == 0x09);
 }
+
+
 
 int MJS_IsDigit(char c) {
  return c >= '0' && c <= '9';
@@ -15,39 +54,43 @@ int MJS_IsDigit(char c) {
 
 int MJS_ParseStringToCache(MJSParsedData *parsed_data) {
  parsed_data->cache_size = 0;
- //FIX : cache has only 512 size of bytes, it might overflow for large strings
- //handle its reallocation
+
+ char *cache_str = (char*)parsed_data->cache;
+ 
  while(parsed_data->current < parsed_data->end) {
   switch(*parsed_data->current) {
    case '\\':
    
-   
-   
    if((parsed_data->current+1) < parsed_data->end) {
    switch(*(++parsed_data->current)) {
     case '\\':
-     parsed_data->cache[parsed_data->cache_size++] = '\\';
+     cache_str[parsed_data->cache_size++] = '\\';
      parsed_data->current++;
     break;
     case 'n':
-     parsed_data->cache[parsed_data->cache_size++] = '\n';
+     cache_str[parsed_data->cache_size++] = '\n';
      parsed_data->current++;
     break;
     case '\"':
-     parsed_data->cache[parsed_data->cache_size++] = '\"';
+     cache_str[parsed_data->cache_size++] = '\"';
      parsed_data->current++;
     break;
     case 'b':
-     parsed_data->cache[parsed_data->cache_size++] = '\b';
+     cache_str[parsed_data->cache_size++] = '\b';
      parsed_data->current++;
     break;
     case 'r':
-     parsed_data->cache[parsed_data->cache_size++] = '\r';
+     cache_str[parsed_data->cache_size++] = '\r';
      parsed_data->current++;
     break;
     case 't':
-     parsed_data->cache[parsed_data->cache_size++] = '\t';
+     cache_str[parsed_data->cache_size++] = '\t';
      parsed_data->current++;
+    break;
+    case 'u': /*unicode*/
+     parsed_data->current++;
+     if(MJS_ReadUnicodeHexadecimal(parsed_data))
+      return -1;
     break;
     default:
      return -1;
@@ -65,19 +108,32 @@ int MJS_ParseStringToCache(MJSParsedData *parsed_data) {
     parsed_data->cl++;
    break;
    default:
-    parsed_data->cache[parsed_data->cache_size++] = *parsed_data->current;
+    cache_str[parsed_data->cache_size++] = *parsed_data->current;
    break;
   }
+  
+  /* expand cache size */
+  if((parsed_data->cache_size+5) > parsed_data->cache_allocated_size) {
+   if(MJSParserData_ExpandCache(parsed_data))
+    return -1;
+  }
+  
   parsed_data->current++;
  }
  __MJS_ParseStringToCache_finish:
- parsed_data->cache[parsed_data->cache_size+1] = '\0';
+ cache_str[parsed_data->cache_size] = '\0';
  return 0;
 }
 
-
-
-unsigned char MJS_ParseNumberToCache(MJSParsedData *parsed_data) {
+/*
+ supported format
+  12 (int)
+  -3 (int)
+  3.45 (float)
+  7e+2 (float)
+  8.3e-1 (float)
+*/
+int MJS_ParseNumberToCache(MJSParsedData *parsed_data) {
  parsed_data->cache_size = 0;
  int is_float = 0;
  int is_negative = 0;
@@ -92,14 +148,14 @@ unsigned char MJS_ParseNumberToCache(MJSParsedData *parsed_data) {
  while(parsed_data->current < parsed_data->end) {
   switch(*parsed_data->current) {
    case '-':
-    if(exponent_part) 
+    if(has_exponent) 
      is_exponent_negative = 1;
     else
      is_negative = 1;
    break;
    case '+':
-    if(exponent_part) 
-     is_exponent_negative = 1;
+    if(has_exponent) 
+     is_exponent_negative = 0;
     else
      is_negative = 0;
    break;
@@ -114,13 +170,26 @@ unsigned char MJS_ParseNumberToCache(MJSParsedData *parsed_data) {
     has_exponent = 1;
    break;
    default:
-    if(!MJS_IsDigit(*parsed_data->current))
-     return 0;
-    else {
+   
+    if(!MJS_IsDigit(*parsed_data->current)) {
+     switch(*parsed_data->current) {
+      case ',':
+      case '}':
+      case ']':
+       goto __MJS_ParseNumberToCache_Output;
+      break;
+      default:
+       if(MJS_IsWhiteSpace(*parsed_data->current))
+        goto __MJS_ParseNumberToCache_Output;
+       return 0;
+      break;
+     }
+    } else {
      if(has_exponent) {
       exponent_part = exponent_part * 10 + (int)(*parsed_data->current - '0');
      } else if(has_fractional) {
       fractional_part = fractional_part * 10 + (int)(*parsed_data->current - '0');
+      fractional_part_count++;
      } else {
       whole_part = whole_part * 10 + (int)(*parsed_data->current - '0');
      }
@@ -130,24 +199,76 @@ unsigned char MJS_ParseNumberToCache(MJSParsedData *parsed_data) {
   parsed_data->current++;
  }
  
+ __MJS_ParseNumberToCache_Output:
+ 
  if(is_float) {
-  float fractional = ((float)fractional_part) * powf(10.0f, (float)-fractional_part_count);
+  float fractional = ((float)fractional_part) * (float)pow(10.0, (double)-fractional_part_count);
   float whole = (float)whole_part;
   float final_value = 0.0f;
   
-  if(has_exponent)
-   final_value = (whole + fractional) * powf(10.0f, (float)(is_exponent_negative ? -exponent_part : exponent_part));
+  if(has_exponent && exponent_part > 0)
+   final_value = (whole + fractional) * (float)pow(10.0, (double)(is_exponent_negative ? -exponent_part : exponent_part));
   else
    final_value = whole + fractional;
+   
+  final_value = is_negative ? -final_value : final_value;
   memcpy(parsed_data->cache, &final_value, sizeof(float));
   parsed_data->cache_size = (unsigned int)sizeof(float);
   return MJS_TYPE_NUMBER_FLOAT;
  } else {
+  whole_part = is_negative ? -whole_part : whole_part;
   memcpy(parsed_data->cache, &whole_part, sizeof(int));
   parsed_data->cache_size = (unsigned int)sizeof(int);
   return MJS_TYPE_NUMBER_INT;
  }
  return 0;
+}
+
+
+int MJS_ReadUnicodeHexadecimal(MJSParsedData *parsed_data) {
+ if((parsed_data->current+4) < parsed_data->end) {
+  unsigned int i, value;
+  value = 0;
+  for (i = 0; i < 4; i++) {
+   const char c = *(parsed_data->current++);
+    value <<= 4;
+    if(c >= '0' && c <= '9')
+     value |= (c - '0');
+    else if (c >= 'A' && c <= 'F')
+     value |= (c - 'A' + 0xA);
+    else if (c >= 'a' && c <= 'f')
+     value |= (c - 'a' + 0xA);
+    else
+     return -1;
+   }
+  parsed_data->current--;
+  parsed_data->cache_size += MJS_UnicodeToChar(value, (char*)parsed_data->cache, parsed_data->cache_size);
+ }
+ return 0;
+}
+
+
+int MJS_UnicodeToChar(unsigned int codepoint, char *out, unsigned int curr) {
+ if(codepoint <= 0x7F) {
+  out[curr++] = codepoint;
+  return 1;
+ } else if(codepoint <= 0x7FF) {
+  out[curr++] = 0xC0 | (codepoint >> 6);
+  out[curr++] = 0x80 | (codepoint & 0x3F);
+  return 2;
+ } else if(codepoint <= 0xFFFF) {
+  out[curr++] = 0xE0 | (codepoint >> 12);
+  out[curr++] = 0x80 | ((codepoint >> 6) & 0x3F);
+  out[curr++] = 0x80 | (codepoint & 0x3F);
+  return 3;
+ } else {
+  out[curr++] = 0xF0 | (codepoint >> 18);
+  out[curr++] = 0x80 | ((codepoint >> 12) & 0x3F);
+  out[curr++] = 0x80 | ((codepoint >> 6) & 0x3F);
+  out[curr++] = 0x80 | (codepoint & 0x3F);
+  return 4;
+ }
+ return 0; /* unreachable */
 }
 
 
