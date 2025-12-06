5599fcfd0883904e1b983b4a0c44a335c246d343
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-12-06 10:30:05 +0800

    update 0.0.6 - 0.0.7

--- src/micro_json/parser_neon.h
+++ src/micro_json/parser_neon.h
@@ -23,15 +23,51 @@ __FORCE_INLINE__ void Neon_ParseStringToCache(MJSParsedData *parsed_data) {
  
  int8x16_t tmp1, tmp2, tmp3;
  
+ /* 16-bit alignment */
+ int ptr_aligned = (int)(((int64_t)parsed_data->current) & 15);
+ 
  /* process 16 characters in one iteration */
- while((parsed_data->current+16) < parsed_data->end) {
+ while((parsed_data->current+32) < parsed_data->end && ptr_aligned) {
   current_16 = vld1q_s8((const signed char*)parsed_data->current);
 
   tmp1 = vceqq_s8(current_16, double_quote_16);
   tmp2 = vceqq_s8(current_16, back_slash_16);
   tmp3 = vceqq_s8(current_16, new_line_16);
   
-  is_equal_16 = vorrq_s8(vorrq_s8(tmp2, tmp3), tmp1);
+  is_equal_16 = vorrq_s8(tmp2, tmp1);
+  
+  parsed_data->cl += Neon_CountNonZero(tmp3);
+
+  if(Neon_AnyOf_s16(is_equal_16)) {
+   /* subdivide the 128-bit vector into two 64-bit vectors */  
+   counter[0] = parsed_data->cache_size;
+   counter[1] = (int64_t)parsed_data->current;
+   counter = Neon_UpdateCounterIfAny(counter, vget_low_s8(is_equal_16), 8);
+   counter = Neon_UpdateCounterIfAny(counter, vget_high_s8(is_equal_16), 8);
+   vst1q_s8((signed char*)&parsed_data->cache[parsed_data->cache_size], current_16);
+
+   parsed_data->cache_size = counter[0];
+   parsed_data->current = (char*)counter[1];
+
+   return;
+  }
+  
+  vst1q_s8((signed char*)&parsed_data->cache[parsed_data->cache_size], current_16);
+  parsed_data->cache_size += 16;
+  parsed_data->current += 16;
+
+
+
+
+  current_16 = vld1q_s8((const signed char*)parsed_data->current);
+
+  tmp1 = vceqq_s8(current_16, double_quote_16);
+  tmp2 = vceqq_s8(current_16, back_slash_16);
+  tmp3 = vceqq_s8(current_16, new_line_16);
+  
+  is_equal_16 = vorrq_s8(tmp2, tmp1);
+  
+  parsed_data->cl += Neon_CountNonZero(tmp3);
 
   if(Neon_AnyOf_s16(is_equal_16)) {
    /* subdivide the 128-bit vector into two 64-bit vectors */  
@@ -53,5 +89,4 @@ __FORCE_INLINE__ void Neon_ParseStringToCache(MJSParsedData *parsed_data) {
  }
 
 }
-
 
--- src/micro_json/token_neon.h
+++ src/micro_json/token_neon.h
@@ -17,17 +17,48 @@ __FORCE_INLINE__ void Neon_tokenize_1(MJSParsedData *parsed_data) {
  int64x2_t counter;
  
  int8x16_t tmp1, tmp2;
+
+ /* 16-bit alignment */
+ int ptr_aligned = (int)(((int64_t)parsed_data->current) & 15);
+
  /* process 16 character per iteration */
- while((parsed_data->current+16) < parsed_data->end) {
+ while((parsed_data->current+32) < parsed_data->end && ptr_aligned) {
   current = vld1q_s8((const signed char*)parsed_data->current);
   
   tmp1 = vceqq_s8(current, new_line_16);
   tmp2 = vceqq_s8(current, curly_brackets_16);
   
-  int8x16_t any_equals = vorrq_s8(tmp1, tmp2);
+  int8x16_t any_equals = tmp2;
   int8x16_t not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
 
   is_equal_16 = vorrq_s8(not_whitespace, any_equals);
+  
+  parsed_data->cl += Neon_CountNonZero(tmp1);
+  
+  if(Neon_AnyOf_s16(is_equal_16)) {
+   /* subdivide the 128-bit vector into two 64-bit vectors */
+   counter[0] = (int64_t)parsed_data->current;
+   counter = Neon_UpdateCounterIfAny(counter, vget_low_s8(is_equal_16), 8);
+   counter = Neon_UpdateCounterIfAny(counter, vget_high_s8(is_equal_16), 8);
+   parsed_data->current = (char*)counter[0];
+   
+   return;
+  }
+  parsed_data->current += 16;
+
+
+
+  current = vld1q_s8((const signed char*)parsed_data->current);
+  
+  tmp1 = vceqq_s8(current, new_line_16);
+  tmp2 = vceqq_s8(current, curly_brackets_16);
+  
+  any_equals = tmp2;
+  not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
+
+  is_equal_16 = vorrq_s8(not_whitespace, any_equals);
+  
+  parsed_data->cl += Neon_CountNonZero(tmp1);
   
   if(Neon_AnyOf_s16(is_equal_16)) {
    /* subdivide the 128-bit vector into two 64-bit vectors */
@@ -57,13 +88,38 @@ __FORCE_INLINE__ void Neon_read_json_object(MJSParsedData *parsed_data) {
  int8x16_t current;
  
  int64x2_t counter;
+
+ /* 16-bit alignment */
+ int ptr_aligned = (int)(((int64_t)parsed_data->current) & 15);
+
  /* process 16 character per iteration */
- while((parsed_data->current+16) < parsed_data->end) {
+ while((parsed_data->current+32) < parsed_data->end && ptr_aligned) {
   current = vld1q_s8((const signed char*)parsed_data->current);
-  int8x16_t any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, new_line_16), vceqq_s8(current, curly_brackets_16)), vceqq_s8(current, double_quote_16)), vceqq_s8(current, colon_16)), vceqq_s8(current, comma_16));
+  int8x16_t any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, curly_brackets_16), vceqq_s8(current, double_quote_16)), vceqq_s8(current, colon_16)), vceqq_s8(current, comma_16));
   int8x16_t not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
   is_equal_16 = vorrq_s8(not_whitespace, any_equals);
   
+  parsed_data->cl += Neon_CountNonZero(vceqq_s8(current, new_line_16));
+
+  if(Neon_AnyOf_s16(is_equal_16)) {
+   /* subdivide the 128-bit vector into two 64-bit vectors */
+   counter[0] = (int64_t)parsed_data->current;
+   counter = Neon_UpdateCounterIfAny(counter, vget_low_s8(is_equal_16), 8 * sizeof(char));
+   counter = Neon_UpdateCounterIfAny(counter, vget_high_s8(is_equal_16), 8 * sizeof(char));
+   parsed_data->current = (char*)counter[0];
+
+   return;
+  }
+  parsed_data->current += 16;
+
+
+  current = vld1q_s8((const signed char*)parsed_data->current);
+  any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, curly_brackets_16), vceqq_s8(current, double_quote_16)), vceqq_s8(current, colon_16)), vceqq_s8(current, comma_16));
+  not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
+  is_equal_16 = vorrq_s8(not_whitespace, any_equals);
+  
+  parsed_data->cl += Neon_CountNonZero(vceqq_s8(current, new_line_16));
+
   if(Neon_AnyOf_s16(is_equal_16)) {
    /* subdivide the 128-bit vector into two 64-bit vectors */
    counter[0] = (int64_t)parsed_data->current;
@@ -96,14 +152,20 @@ __FORCE_INLINE__ void Neon_read_json_object_value(MJSParsedData *parsed_data) {
  int8x16_t current;
  
  int64x2_t counter;
+
+ /* 16-bit alignment */
+ int ptr_aligned = (int)(((int64_t)parsed_data->current) & 15);
+
  /* process 16 character per iteration */
- while((parsed_data->current+16) < parsed_data->end) {
+ while((parsed_data->current+32) < parsed_data->end && ptr_aligned) {
   current = vld1q_s8((const signed char*)parsed_data->current);
-  int8x16_t any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, new_line_16), vceqq_s8(current, t_16)), vceqq_s8(current, f_16)), vceqq_s8(current, n_16)), vceqq_s8(current, double_quote_16));
+  int8x16_t any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, t_16), vceqq_s8(current, f_16)), vceqq_s8(current, n_16)), vceqq_s8(current, double_quote_16));
   any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, plus_16), vceqq_s8(current, minus_16)), vceqq_s8(current, open_curly_brackets_16)), vceqq_s8(current, close_curly_brackets_16)), vceqq_s8(current, comma_16)), vceqq_s8(current, open_square_brackets_16));
   int8x16_t not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
   is_equal_16 = vorrq_s8(vorrq_s8(Neon_IsDigit_s16(current), any_equals), not_whitespace);
- 
+
+  parsed_data->cl += Neon_CountNonZero(vceqq_s8(current, new_line_16));
+
   if(Neon_AnyOf_s16(is_equal_16)) {
    /* subdivide the 128-bit vector into two 64-bit vectors */
    counter[0] = (int64_t)parsed_data->current;
@@ -114,5 +176,26 @@ __FORCE_INLINE__ void Neon_read_json_object_value(MJSParsedData *parsed_data) {
    return;
   }
   parsed_data->current += 16;
+
+
+  current = vld1q_s8((const signed char*)parsed_data->current);
+  any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, t_16), vceqq_s8(current, f_16)), vceqq_s8(current, n_16)), vceqq_s8(current, double_quote_16));
+  any_equals = vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vorrq_s8(vceqq_s8(current, plus_16), vceqq_s8(current, minus_16)), vceqq_s8(current, open_curly_brackets_16)), vceqq_s8(current, close_curly_brackets_16)), vceqq_s8(current, comma_16)), vceqq_s8(current, open_square_brackets_16));
+  not_whitespace = veorq_s8(Neon_IsWhitespace_s16(current), all_ones_16);
+  is_equal_16 = vorrq_s8(vorrq_s8(Neon_IsDigit_s16(current), any_equals), not_whitespace);
+
+  parsed_data->cl += Neon_CountNonZero(vceqq_s8(current, new_line_16));
+
+  if(Neon_AnyOf_s16(is_equal_16)) {
+   /* subdivide the 128-bit vector into two 64-bit vectors */
+   counter[0] = (int64_t)parsed_data->current;
+   counter = Neon_UpdateCounterIfAny(counter, vget_low_s8(is_equal_16), 8 * sizeof(char));
+   counter = Neon_UpdateCounterIfAny(counter, vget_high_s8(is_equal_16), 8 * sizeof(char));
+   parsed_data->current = (char*)counter[0];
+   
+   return;
+  }
+  parsed_data->current += 16;
+ 
  }
 }
